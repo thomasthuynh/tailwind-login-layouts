1f52c2195e7ccff565f39ca916d93026e37dfa2a
ThomasHuynh <Thomas_TTH@live.com>
2023-11-12 21:50:04 -0500

    Styled third login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Login from "./components/Login"
 import Login2 from "./components/Login2"
+import Login3 from "./components/Login3"
 
 function App() {
 
   return (
     <div>
-      {/* <Login /> */}
-      <Login2 />
+      {/* <Login />
+      <Login2 /> */}
+      <Login3 />
     </div>
   )
 }
